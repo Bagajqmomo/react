99d872080c79add10794e0b8382dcb0bef816a42
CY <changyun.hang@izyoo.com>
2024-11-05 18:59:00 +0800

    do booklist

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 import React from "react";
 import reactDOM from "react-dom/client";
 
-const Greeting = () => {
+const Booklist = () => {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+    </section>
   );
 };
 
-const Person = () => <h2>Cendol</h2>;
-const Message = () => {
-  return <p>Fuck Gordon Cheah</p>;
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/71AQWDUx1bL._SY466_.jpg"
+      alt="The Housemaid's Wedding: A Short Story "
+    />
+  );
+};
+
+const Title = () => {
+  return <h2>The Housemaid's Wedding: A Short Story </h2>;
+};
+const Author = () => {
+  return <h4>Author Name</h4>;
 };
 
 const root = reactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<Booklist />);
